16329eb34e8332927a330b9ac1d8a218f7f02c0f
Vellmond <edgarbautistaz3@gmail.com>
2023-01-29 23:42:51 -0600

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import NavButton from "./NavButton";
+
+function Header() {
+  return (
+        <div>
+            <div className={"flex items-center space-x-2"}>
+                <img
+                    className={"rounded-full h-20 w-20"}
+                    src={"https://www.pngmart.com/files/13/Halo-Game-PNG-Picture.png"}
+                    alt=""
+                />
+            </div>
+            <div>
+                <h1 className={"text-lg text-white font-bold"}>Vellmond Draw</h1>
+                <p className={"text-xs text-emerald-500 truncate"}>User...</p>
+            </div>
+            <div>
+                <div className={"bg-emerald-900"}>
+                    <NavButton/>
+                    <NavButton/>
+
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default Header;
---
+++ components/NavButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface Props {
+    title: string;
+}
+
+function NavButton({title}: Props) {
+        return <button>{title}</button>;
+
+}
+
+export default NavButton;
